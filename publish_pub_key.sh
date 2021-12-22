8bce32eb81d56ceecb450a5abeddc4c792df69e5
vanquisher <32311274+Prajwalmithun@users.noreply.github.com>
2021-12-22 14:15:34 +0530

    Update publish_pub_key.sh

--- publish_pub_key.sh
+++ publish_pub_key.sh
@@ -3,7 +3,17 @@
 ## Author: Prajwal 
 ## Email: tkushal216@gmail.com
 ## Last revised: 2021/12/22
-## Usage: ./publish_pub_key.sh FULL_PATH_OF_HOSTS_DETAIL_FILE 
+##
+## Usage: ./publish_pub_key.sh FULL_PATH_OF_HOSTS_DETAIL_FILE
+## 
+## 
+## Example of HOST file
+<<comment
+10.10.10.1,user1,pass1
+10.10.10.2,user2,pass2
+10.10.10.3,user3,pass3
+comment
+
 
 host_file=$1
 
